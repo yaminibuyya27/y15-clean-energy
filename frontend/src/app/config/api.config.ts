a56795b92720ce4c2f9bfebd889db2a1ec016be5
Yamini Buyya <yaminibuyya27@gmail.com>
2025-11-30 20:14:11 -0500

    update api config file in frontend

--- frontend/src/app/config/api.config.ts
+++ frontend/src/app/config/api.config.ts
@@ -1,3 +1,4 @@
-window.location.hostname === 'localhost' 
-  ? 'http://localhost:3000' 
-  : '/api';
+export const API_URL = 
+  window.location.hostname === 'localhost' 
+    ? 'http://localhost:3000' 
+    : '';
