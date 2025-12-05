4cbdfeaddc728fd08d3642e3ef9ab6ec1471181e
Yamini Buyya <yaminibuyya27@gmail.com>
2025-12-05 18:57:24 -0500

    Update api path in frontend

--- frontend/src/app/config/api.config.ts
+++ frontend/src/app/config/api.config.ts
@@ -1,4 +1,1 @@
-export const API_URL = 
-  window.location.hostname === 'localhost' 
-    ? 'http://localhost:3000' 
-    : '';
+export const API_URL = 'http://' + window.location.hostname + ':3000';
